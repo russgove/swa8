a306fc450fe1b41322ed9eac63ff48e3919b1803
Russell gove <russellgove@gmail.com>
2020-10-08 18:21:14 -0400

    fabric

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Label } from 'office-ui-fabric-react/lib/Label';
+import { DatePicker } from 'office-ui-fabric-react/lib/DatePicker';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
+
+import { MessageBar, MessageBarType, } from 'office-ui-fabric-react/lib/MessageBar';
+import { CommandBarButton } from 'office-ui-fabric-react/lib/Button';
+import { CommandBar, ICommandBarItemProps } from 'office-ui-fabric-react/lib/CommandBar';
+import { DetailsList, DetailsListLayoutMode, IColumn } from 'office-ui-fabric-react/lib/DetailsList';
+import { PrimaryButton, Button, ActionButton } from 'office-ui-fabric-react/lib/Button';
 
 function App() {
   return (
@@ -8,7 +17,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        c'mon man!, Edit <code>src/App.tsx</code> and save to reload,!
+        <DatePicker isRequired={true} label="Closing Date" placeholder="enter closing Date" 
+       
+            />
+      <Label >c'mon man!, Edit <code>src/App.tsx</code> and save to reload,!</Label>  
         
         </p>
         <a
