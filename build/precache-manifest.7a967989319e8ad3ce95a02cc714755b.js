47bccc61ff8b0d85a5b3b668318462ccfe2b2a6e
Russell gove <russellgove@gmail.com>
2020-10-08 16:10:39 -0400

    ww

---
+++ build/precache-manifest.7a967989319e8ad3ce95a02cc714755b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049736412b5a974df3f4bc7017917d07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d996b3781c1d490f25",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52d870c599a4ee57fe19",
+    "url": "/static/js/2.d2036c77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d2036c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d996b3781c1d490f25",
+    "url": "/static/js/main.6eb7ed44.chunk.js"
+  },
+  {
+    "revision": "0316bcef328d14076106",
+    "url": "/static/js/runtime-main.4f3704f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
