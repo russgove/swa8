b07362471beea76ff19511e8ef90bfe41382e542
Russell gove <russellgove@gmail.com>
2020-10-08 18:29:37 -0400

    rmove unused

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Label } from 'office-ui-fabric-react/lib/Label';
 import { DatePicker } from 'office-ui-fabric-react/lib/DatePicker';
-import { TextField } from 'office-ui-fabric-react/lib/TextField';
 
-import { MessageBar, MessageBarType, } from 'office-ui-fabric-react/lib/MessageBar';
-import { CommandBarButton } from 'office-ui-fabric-react/lib/Button';
-import { CommandBar, ICommandBarItemProps } from 'office-ui-fabric-react/lib/CommandBar';
-import { DetailsList, DetailsListLayoutMode, IColumn } from 'office-ui-fabric-react/lib/DetailsList';
-import { PrimaryButton, Button, ActionButton } from 'office-ui-fabric-react/lib/Button';
 
 function App() {
   return (
