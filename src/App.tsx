2b1620728e1fed7761e03cef8ccabd157252e400
Russell gove <russellgove@gmail.com>
2020-10-08 17:03:11 -0400

    c'mon

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload, c'mon man!
+        c'mon man!, Edit <code>src/App.tsx</code> and save to reload, c'mon man!
         
         </p>
         <a
